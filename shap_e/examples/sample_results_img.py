ad4e68f20244fed06442fde9264666346b27cab3
edward1997104 <edward1997104@link.cuhk.edu.hk>
2023-11-02 20:07:55 +0800

    1. commit changes

--- shap_e/examples/sample_results_img.py
+++ shap_e/examples/sample_results_img.py
@@ -1,12 +1,35 @@
 import torch
 import os
+import trimesh
 from shap_e.diffusion.sample import sample_latents
 from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
 from shap_e.models.download import load_model, load_config
 from shap_e.util.notebooks import decode_latent_mesh
 from shap_e.util.image_util import load_image
+import math
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def rotate_around_axis(mesh, axis = 'x', reverse = False):
+    if reverse:
+        angle = math.pi / 2
+    else:
+        angle = -math.pi / 2
+
+    if axis == 'x':
+        direction = [1, 0, 0]
+    elif axis == 'y':
+        direction = [0, 1, 0]
+    else:
+        direction = [0, 0, 1]
+
+    center = mesh.centroid
+
+    rot_matrix = trimesh.transformations.rotation_matrix(angle, direction, center)
+
+    mesh.apply_transform(rot_matrix)
+
+    return mesh
 
 if __name__ == '__main__':
     output_dir = 'shape-mesh-output'
@@ -38,11 +61,11 @@ if __name__ == '__main__':
 
     mesh = decode_latent_mesh(xm=xm, latent=latents[0])
     mesh = mesh.tri_mesh()
+    mesh = trimesh.Trimesh(vertices=mesh.verts, faces=mesh.faces)
+    mesh = rotate_around_axis(mesh, axis = 'x', reverse = False)
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
-    with open(
-            f'{output_dir}/testing.obj', 'w'
-    ) as f:
-        mesh.write_obj(f)
+    mesh.export(f'{output_dir}/testing.obj')
+
